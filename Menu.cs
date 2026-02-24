bdf8edf5650c63a80389730f829f61418fa104cc
matheus sanderhus <sanderhus15@outlook.com>
2026-02-24 13:23:35 -0300

    Adicionando funcoes

--- Menu.cs
+++ Menu.cs
@@ -8,42 +8,48 @@ namespace EditorHtml
         {
             Console.Clear();
             Console.BackgroundColor = ConsoleColor.Black;
-            Console.ForegroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.White;
 
             DrawScreen();
         }
 
         public static void DrawScreen()
         {
+
+            ImprimirMais();
+            BarraLateral();
+            ImprimirMais();
+        }
+
+        static void ImprimirMais()
+        {
             Console.Write("+");
             for(int i = 0; i <= 30; i++)
             {
-                Console.WriteLine("-");
+                Console.Write("-");
             }
             Console.Write("+");
             Console.Write("\n");
+            
+        }
 
+        static void BarraLateral()
+        {
             for(int lines = 0; lines <= 10; lines++)
             {
                 Console.Write("|");
 
                 for(int i = 0; i <= 30; i++)
                 {   
-                    Console.WriteLine(" ");
+                    Console.Write(" ");
                 }
 
                 Console.Write("|");
                 Console.Write("\n");
             }
-
-            Console.Write("+");
-            for(int i = 0; i <= 30; i++)
-            {
-                Console.WriteLine("-");
-            }
-            Console.Write("+");
-            Console.Write("\n");
+            
         }
+        
         
     }
 }
